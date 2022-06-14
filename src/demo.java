97386794d746715a40adab9d14a193e1b6d03b98
devanshu <devanshu.kr.jha@gmail.com>
2022-06-14 16:09:48 +0530

    New functionality added to java class demo.

--- src/demo.java
+++ src/demo.java
@@ -1,6 +1,8 @@
 public class demo {
     public static void main(String[] args) {
         skip("","Avnegers",'e',0);
+       String str = "Assassination";
+       printRev(str, str.length()-1);
     }
     static void skip(String p, String up, char ch, int idx){
         if (idx == up.length()){
@@ -13,5 +15,14 @@ public class demo {
             p += up.charAt(idx);
             skip(p,up,ch,idx+1);
         }
+    }
+    static void printRev(String str, int idx){
+        if (idx == 0){
+            System.out.print(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        printRev(str, idx-1);
+
     }
 }
